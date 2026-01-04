1bb7d217d3b2331e846b0d2e7bdd18a73ae63e76
saifur-sagor <mdsaifurrahmansagor606@gmail.com>
2026-01-04 12:47:59 +0600

    navbar fixed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="navbar bg-blue-950 shadow-md px-4">
+    <div className="navbar sticky z-10 top-0 bg-blue-950 shadow-md px-4">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
